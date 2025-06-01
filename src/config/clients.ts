ff5f74f2129b19e65a535e45a75c511addb6005a
Gabriel Ciobanu <gabriel97sy@Ryujintosh.local>
2025-06-01 18:43:47 +0300

    updated configs, README

--- src/config/clients.ts
+++ src/config/clients.ts
@@ -5,6 +5,14 @@ import { ConfigBuilder } from './configBuilder';
 export const config: ClientsEndpointsAlgorithmsConfig = new ConfigBuilder()
   .set('client-1', 'bar', 'token-bucket', {
     capacity: 3,
+    tokenRegenerationRate: 1 / MS_PER_SECOND,
+  })
+  .set('client-1', 'foo', 'sliding-window-log', {
+    windowMs: 5 * MS_PER_SECOND,
+    limit: 2,
+  })
+  .set('client-2', 'bar', 'token-bucket', {
+    capacity: 2,
     tokenRegenerationRate: 0.5 / MS_PER_SECOND,
   })
   .set('client-2', 'foo', 'sliding-window-log', {
